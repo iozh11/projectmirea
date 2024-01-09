06536bc3b828526f333e14be705858b9d0a11381
iozh11 <135107119+iozh11@users.noreply.github.com>
2024-01-10 02:39:43 +0300

    1.2

--- proektmirea/main/urls.py
+++ proektmirea/main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('fridrih_engels', views.fridrih_engels)
+    path('', views.index, name = "home"),
+    path('fridrih_engels', views.fridrih_engels, name = "f_eng"),
+    path('karl_marks', views.karl_marks, name = "k_mar"),
+    path('ilych_lenin', views.ilych_lenin, name = "i_len"),
 ]
--- proektmirea/main/views.py
+++ proektmirea/main/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def fridrih_engels(request):
     return render(request, 'main/fridrih_engels.html')
+
+def karl_marks(request):
+    return render(request, 'main/karl_marks.html')
+
+def ilych_lenin(request):
+    return render(request, 'main/ilych_lenin.html')
